ddbaec0022f8214316e74ed22c6423a6853316f7
Joao-Peterson <jooxpeters@gmail.com>
2020-10-26 11:19:05 -0300

    Colors!

--- ascii_screen.h
+++ ascii_screen.h
@@ -1,7 +1,7 @@
 #include <stdlib.h>
-#include <stdarg.h>
-#include <Windows.h>
+#include <stdbool.h>
 #include <string.h>
+#include <Windows.h>
 
 /* ------------------ */
 
@@ -22,6 +22,34 @@ typedef enum{
     BR  =   4
 }ANCHOR_Typedef;
 
+typedef enum{
+    ASCII_COLOR_FOREGROUND_BLUE         = 0x01,
+    ASCII_COLOR_FOREGROUND_GREEN        = 0x02,
+    ASCII_COLOR_FOREGROUND_RED          = 0x04,
+    
+    ASCII_COLOR_FOREGROUND_BLACK        = 0x00,
+    ASCII_COLOR_FOREGROUND_WHITE        = 0x07,
+    ASCII_COLOR_FOREGROUND_INTENSITY    = 0x08,
+    
+    ASCII_COLOR_FOREGROUND_CYAN         = 0x01 | 0x02,
+    ASCII_COLOR_FOREGROUND_YELLOW       = 0x02 | 0x04,
+    ASCII_COLOR_FOREGROUND_MAGENTA      = 0x01 | 0x04,
+
+
+    ASCII_COLOR_BACKGROUND_BLUE         = 0x10,
+    ASCII_COLOR_BACKGROUND_GREEN        = 0x20,
+    ASCII_COLOR_BACKGROUND_RED          = 0x40,
+    
+    ASCII_COLOR_BACKGROUND_BLACK        = 0x00,
+    ASCII_COLOR_BACKGROUND_WHITE        = 0x70,
+    ASCII_COLOR_BACKGROUND_INTENSITY    = 0x80,   
+    
+    ASCII_COLOR_BACKGROUND_CYAN         = 0x10 | 0x20,
+    ASCII_COLOR_BACKGROUND_YELLOW       = 0x20 | 0x40,
+    ASCII_COLOR_BACKGROUND_MAGENTA      = 0x10 | 0x40
+
+}ASCII_COLOR_Typedef;
+
 /* ------------------ */
 
 ascii_screen *ascii_screen_new(unsigned int width, unsigned int height);
@@ -32,35 +60,41 @@ void ascii_screen_display_buffer(ascii_screen* screen);
 
 void ascii_screen_draw_sprite(ascii_screen *screen, ANCHOR_Typedef anchor, unsigned int x, unsigned int y, ascii_sprite *sprite);
 
+void ascii_screen_fill_screen(ascii_screen *screen, char character, ASCII_COLOR_Typedef color);
+
 void ascii_screen_clear(ascii_screen *screen);
 
-ascii_sprite **ascii_sprite_new(unsigned int width, unsigned int height, unsigned int files_qty, ...);
+ascii_sprite **ascii_sprite_new(unsigned int width, unsigned int height, unsigned int files_qty, char **array, bool no_background, ASCII_COLOR_Typedef background_color, ASCII_COLOR_Typedef font_color);
 
-void ascii_sprite_delete(ascii_sprite *sprite_array);
+void ascii_sprite_delete(ascii_sprite **sprite_array);
 
 /* ------------------ */
 
 struct ascii_screen{
-    int w;
-    int h;
+    int width;
+    int height;
     int size;
-    char *buffer_stream;
-    char **buffer_mat;
+    CHAR_INFO *stream;
+    CHAR_INFO **buffer;
     HANDLE hscreenbuffer;
 
     void (*Display_buffer) (ascii_screen* screen);
     void (*Delete) (ascii_screen *screen);
     void (*Draw_sprite) (ascii_screen *screen, ANCHOR_Typedef anchor, unsigned int x, unsigned int y, ascii_sprite *sprite);
+    void (*Fill_screen) (ascii_screen *screen, char character, ASCII_COLOR_Typedef color);
     void (*Clear) (ascii_screen *screen);
 } ;
 
 struct ascii_sprite{
     int width;
     int height;
-    int s_qty;
-    char** sprite_mat;
+    int sprite_qty;
+    WORD background_color;
+    bool no_background;
+    WORD font_color;
+    CHAR_INFO **sprite;
 
-    void (*Delete) (ascii_sprite *sprite_array);
+    void (*Delete) (ascii_sprite **sprite_array);
 };
 
 /* ------------------ */
@@ -77,10 +111,10 @@ ascii_screen *ascii_screen_new(unsigned int width, unsigned int height){
     SMALL_RECT w_size = { 0, 0, w_width, w_height};
     COORD b_size = {b_width, b_height};
 
-    char *buffer_stream = (char*)calloc(b_height * b_width + 1, sizeof(char)); // char string stream
+    CHAR_INFO *buffer_stream = (CHAR_INFO*)calloc(b_height * b_width + 1, sizeof(CHAR_INFO)); // char string stream
     memset(buffer_stream, 0x20, width * height);
 
-    char **buffer_mat = (char**)calloc(b_height, sizeof(char*)); // pointer array for acessing the stream
+    CHAR_INFO **buffer_mat = (CHAR_INFO**)calloc(b_height, sizeof(CHAR_INFO*)); // pointer array for acessing the stream
 
     for(int i = 0; i < b_height; i++) // pointers of the array
         buffer_mat[i] = buffer_stream + (i * b_width);
@@ -92,15 +126,16 @@ ascii_screen *ascii_screen_new(unsigned int width, unsigned int height){
     SetConsoleWindowInfo(hscreenbuffer, TRUE, &w_size);
     SetConsoleMode(hscreenbuffer, ENABLE_WRAP_AT_EOL_OUTPUT | ENABLE_PROCESSED_OUTPUT);
 
-    scr->w = width;
-    scr->h = height;
+    scr->width = width;
+    scr->height = height;
     scr->size = width * height;
     scr->hscreenbuffer = hscreenbuffer;
-    scr->buffer_stream = buffer_stream;
-    scr->buffer_mat = buffer_mat;
+    scr->stream = buffer_stream;
+    scr->buffer = buffer_mat;
     scr->Delete = ascii_screen_delete;
     scr->Display_buffer = ascii_screen_display_buffer;
     scr->Draw_sprite = ascii_screen_draw_sprite;
+    scr->Fill_screen = ascii_screen_fill_screen;
     scr->Clear = ascii_screen_clear;
 
     return scr;
@@ -109,15 +144,15 @@ ascii_screen *ascii_screen_new(unsigned int width, unsigned int height){
 void ascii_screen_delete(ascii_screen *screen){
     CloseHandle(screen->hscreenbuffer);
 
-    free(screen->buffer_mat);
-    free(screen->buffer_stream);
+    free(screen->buffer);
+    free(screen->stream);
     free(screen);
 }
 
 void ascii_screen_display_buffer(ascii_screen* screen){
-    unsigned long a;
-    screen->buffer_stream[screen->size] = '\0';
-    WriteConsoleOutputCharacterA(screen->hscreenbuffer, screen->buffer_stream, screen->size, (COORD){0,0}, &a);
+    SMALL_RECT a = {.Top = 0, .Left = 0, .Bottom = screen->height, .Right = screen->width};
+    screen->stream[screen->size].Char.AsciiChar = '\0';
+    WriteConsoleOutputA(screen->hscreenbuffer, screen->stream, (COORD){screen->width, screen->height}, (COORD){0,0}, &a);
 }
 
 void ascii_screen_draw_sprite(ascii_screen *screen, ANCHOR_Typedef anchor, unsigned int x, unsigned int y, ascii_sprite *sprite){ 
@@ -150,24 +185,31 @@ void ascii_screen_draw_sprite(ascii_screen *screen, ANCHOR_Typedef anchor, unsig
     for(int i = 0; i < sprite->height; i++){
         for(int j = 0; j < sprite->width; j++){
             
-            if(((i + delta_y) >= 0 && (i + delta_y) < screen->h) && ((j + delta_x) >= 0 && (j + delta_x) < screen->w)) // inside the screen
-                if(sprite->sprite_mat[i][j] != ' ') // when ' ', space, dont print
-                    screen->buffer_mat[i + delta_y][j + delta_x] = sprite->sprite_mat[i][j];
+            if(((i + delta_y) >= 0 && (i + delta_y) < screen->height) && ((j + delta_x) >= 0 && (j + delta_x) < screen->width)) // inside the screen
+                if(sprite->sprite[i][j].Char.AsciiChar != ' ' || sprite->no_background == 0) // when ' ', space, dont print, except when no_background is 0, then print
+                    screen->buffer[i + delta_y][j + delta_x] = sprite->sprite[i][j];
 
         }
     }    
 }
 
+void ascii_screen_fill_screen(ascii_screen *screen, char character, ASCII_COLOR_Typedef color){
+    CHAR_INFO clear_pixel = {.Char.AsciiChar = character, .Attributes = color};
+
+    for(int i = 0; i < screen->height; i++)
+        for(int j = 0; j < screen->width; j++)
+            screen->buffer[i][j] = clear_pixel;        
+}
+
 void ascii_screen_clear(ascii_screen *screen){
-    memset(screen->buffer_stream, 0x20, screen->size);
+    ascii_screen_fill_screen(screen, ' ', 0);
 }
 
 
 
-ascii_sprite **ascii_sprite_new(unsigned int width, unsigned int height, unsigned int files_qty, ...){
-    va_list args;
-    va_start(args,files_qty);
 
+ascii_sprite **ascii_sprite_new(unsigned int width, unsigned int height, unsigned int files_qty, char **array, bool no_background, ASCII_COLOR_Typedef background_color, ASCII_COLOR_Typedef font_color){
+    
     ascii_sprite **sprite_array = (ascii_sprite**)calloc(files_qty,sizeof(ascii_sprite*));
     ascii_sprite *sprite_cursor;
 
@@ -180,31 +222,35 @@ ascii_sprite **ascii_sprite_new(unsigned int width, unsigned int height, unsigne
         sprite_cursor = sprite_array[i];
         
 
-        sprite_string = va_arg(args, char*);
+        sprite_string = array[i];
 
-        sprite_cursor->sprite_mat = (char**)calloc(height, sizeof(char*));
+        sprite_cursor->sprite = (CHAR_INFO**)calloc(height, sizeof(CHAR_INFO*));
 
         for(int j = 0; j < height; j ++){ // for each line
         
-            sprite_cursor->sprite_mat[j] = (char*)calloc(width, sizeof(char));
+            sprite_cursor->sprite[j] = (CHAR_INFO*)calloc(width, sizeof(CHAR_INFO));
 
             int is_inline = 1;
             int is_insprite = 1;
 
             for(int k = 0; k < width; k++){ // for each column
                 if( !is_insprite ){
-                    sprite_cursor->sprite_mat[j][k] = ' ';
+                    sprite_cursor->sprite[j][k].Char.AsciiChar = ' ';
+                    sprite_cursor->sprite[j][k].Attributes = (WORD) background_color;
                 }
                 else if( (sprite_string[k] != '\n' && sprite_string[k] != '\0') && is_inline){ // reads until a '\n' or '\0' occur, in this case fill with ' ' spaces;
-                    sprite_cursor->sprite_mat[j][k] = sprite_string[k];
+                    sprite_cursor->sprite[j][k].Char.AsciiChar = sprite_string[k];
+                    sprite_cursor->sprite[j][k].Attributes = (WORD) font_color;
                 }
                 else if( sprite_string[k] == '\0' ){
-                    sprite_cursor->sprite_mat[j][k] = ' ';
+                    sprite_cursor->sprite[j][k].Char.AsciiChar = ' ';
+                    sprite_cursor->sprite[j][k].Attributes = (WORD) background_color;
                     is_inline = 0;
                     is_insprite = 0;
                 }
                 else{
-                    sprite_cursor->sprite_mat[j][k] = ' ';
+                    sprite_cursor->sprite[j][k].Char.AsciiChar = ' ';
+                    sprite_cursor->sprite[j][k].Attributes = (WORD) background_color;
                     is_inline = 0;
                 }
             }
@@ -220,27 +266,31 @@ ascii_sprite **ascii_sprite_new(unsigned int width, unsigned int height, unsigne
             }
         }
 
-        sprite_cursor->s_qty = files_qty;
+        sprite_cursor->sprite_qty = files_qty;
         sprite_cursor->width = width;
         sprite_cursor->height = height;
+        sprite_cursor->no_background = no_background;
         sprite_cursor->Delete = ascii_sprite_delete;
     }
-
-    va_end(args);
 
     return sprite_array;
 }
 
-void ascii_sprite_delete(ascii_sprite *sprite_array){
-    ascii_sprite *cursor;
-    for (int i = 0; i < sprite_array->s_qty; i++){
-        cursor = sprite_array + i;
+void ascii_sprite_delete(ascii_sprite **sprite_array){
+    int s_qty = sprite_array[0]->sprite_qty;
+    int height = sprite_array[0]->height;
 
-        for(int j = 0; j < cursor->height; j++){
-            free(cursor->sprite_mat[j]);    
+    for (int i = 0; i < s_qty; i++){
+        
+        for(int j = 0; j < height; j++){
+            free(sprite_array[i]->sprite[j]);    
         }
         
-        free(cursor->sprite_mat);
-        free(cursor);
+        free(sprite_array[i]->sprite);
+        free(sprite_array[i]);
     }
+
+    free(sprite_array);
 }
+
+// IMPLEMENT A RE COLOURING FUNCTION AND SEPARATE SPRITE FUNCTIONS BY ARRAY/SINGLE.
--- main.c
+++ main.c
@@ -29,24 +29,33 @@
 // 0xDB
 
 
-int main(void){
-
-    char *rex_0 = get_win_resource_binary_data("rex_0");
-    char *rex_1 = get_win_resource_binary_data("rex_1");
-    char *rex_2 = get_win_resource_binary_data("rex_2");
-    char *rex_3 = get_win_resource_binary_data("rex_3");
-
-    ascii_sprite** rex_sprite = ascii_sprite_new(9,6, 4, rex_0, rex_1, rex_2, rex_3);
-
-    ascii_screen *console = ascii_screen_new(77, 17);
+int main(void) {
 
     float time_ratio = 1.0;
     float time;
     float time_counter = 0.0;
     clock_t time_stamp;
     clock_t time_elapsed;
+    
+    ASCII_COLOR_Typedef sky = ASCII_COLOR_BACKGROUND_BLUE | ASCII_COLOR_BACKGROUND_INTENSITY;
 
     int sprite_select = 1;
+
+    char **rex_sprites_string = (char**)malloc(sizeof(char*)*4);
+    rex_sprites_string[0] = get_win_resource_binary_data("rex_0");
+    rex_sprites_string[1] = get_win_resource_binary_data("rex_1");
+    rex_sprites_string[2] = get_win_resource_binary_data("rex_2");
+    rex_sprites_string[3] = get_win_resource_binary_data("rex_3");
+
+
+    ascii_sprite** rex_sprite = ascii_sprite_new(9,6, 4, rex_sprites_string, true, sky, sky | ASCII_COLOR_FOREGROUND_GREEN | ASCII_COLOR_FOREGROUND_INTENSITY);
+
+    for(int i = 0; i < 4; i++)
+        free(rex_sprites_string[i]);
+    free(rex_sprites_string);
+
+    ascii_screen *console = ascii_screen_new(77, 17);
+
 
     while(1)
     {
@@ -64,12 +73,15 @@ int main(void){
             time_counter = 0.0;
         }
 
-        console->Clear;
-        console->Draw_sprite(console,ANCHOR_TOP_LEFT,(console->w - rex_sprite[0]->width) / 2, (console->h - rex_sprite[0]->height) / 2,rex_sprite[sprite_select]);
+        console->Fill_screen(console, ' ', sky);
+        console->Draw_sprite(console,ANCHOR_TOP_LEFT,(console->width - rex_sprite[0]->width) / 2, (console->height - rex_sprite[0]->height) / 2,rex_sprite[sprite_select]);
         console->Display_buffer(console);
                 
         time_elapsed = clock() - time_stamp;
     }
+
+    console->Delete(console);
+    rex_sprite[0]->Delete(rex_sprite);
 
     return 0;
 }
